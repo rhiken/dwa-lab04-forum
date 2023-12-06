116214a56677ead08d6a73a4978d55450d5088fa
JBond <128589189+rhiken@users.noreply.github.com>
2023-12-06 07:30:29 +0000

    add comments

--- routes/main.js
+++ routes/main.js
@@ -1,23 +1,37 @@
 module.exports = function (app, forumData) {
     // Handle our routes
+
+    // Home page
     app.get("/", function (req, res) {
       res.render("index.ejs", forumData);
     });
+
+    // About page
     app.get("/about", function (req, res) {
        res.render("about.ejs", forumData);
     });
+
+    // Add Post Page
     app.get("/addpost", function (req, res) {
         res.render("addpost.ejs", forumData);
     });
+
+    // Post Page
     app.get("/posts", function (req, res) {
         res.render("posts.ejs", forumData);
     });
+
+    // Search Post Page
     app.get("/searchpost", function (req, res) {
         res.render("searchpost.ejs", forumData);
     });
+
+    // Topic Page
     app.get("/topics", function (req, res) {
         res.render("topics.ejs", forumData);
     });
+
+    // User Page
     app.get("/users", function (req, res) {
         res.render("users.ejs", forumData);
     });
