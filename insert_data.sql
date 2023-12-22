6923cca32f2ba0bd6e487a090a1d27c0cad90b7a
rhiken <rkenn004@gold.ac.uk>
2023-12-22 12:36:17 +0000

    add: dummy data

---
+++ insert_data.sql
@@ -0,0 +1,19 @@
+USE myForum;
+
+INSERT INTO `user` (`username`, `topic_privileges`) VALUES
+('Amy', 1),
+('Benedict', 2),
+('Charlie', 3),
+('Danny', 1),
+('Edward', 2);
+
+INSERT INTO `topic` (`name`) VALUES
+('DIY'),
+('Film'),
+('Gardening'),
+('Sport'),
+('TV');
+
+INSERT INTO `post` (`title`, `content`, `userid`, `topicid`, `date`, `username`) VALUES
+('Replacing drill', 'Any recommendations?', 1, 1, '2023-01-01', 'Amy'),
+
