d02f63e6090400f18c8e51a09e88ebdec06fcc0b
rhiken <rkenn004@gold.ac.uk>
2023-12-22 14:04:54 +0000

    remove constraints

--- create_db.sql
+++ create_db.sql
@@ -1,23 +1,22 @@
 # Create database script for Thought Exchange
 
 # Create the database
-CREATE DATABASE IF NOT EXISTS `myforum`;
-USE `myforum`;
+CREATE DATABASE IF NOT EXISTS myforum;
+USE myforum;
 
 # Create the tables
 
-CREATE TABLE `user` (
+CREATE TABLE user (
   `user_id` int NOT NULL AUTO_INCREMENT,
   `username` varchar(20) DEFAULT NULL,
   `topic_privileges` int DEFAULT NULL,
   PRIMARY KEY (`user_id`),
   UNIQUE KEY `user_id_UNIQUE` (`user_id`),
   UNIQUE KEY `username_UNIQUE` (`username`),
-  KEY `FK_user_topicid_idx` (`topic_privileges`),
-  CONSTRAINT `FK_user_topicid` FOREIGN KEY (`topic_privileges`) REFERENCES `topic` (`topic_id`)
+  KEY `FK_user_topicid_idx` (`topic_privileges`)
 );
 
-CREATE TABLE `topic` (
+CREATE TABLE topic (
   `topic_id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`topic_id`),
@@ -25,7 +24,7 @@ CREATE TABLE `topic` (
   UNIQUE KEY `topic_name_UNIQUE` (`name`)
 );
 
-CREATE TABLE `post` (
+CREATE TABLE post (
   `post_id` int NOT NULL AUTO_INCREMENT,
   `title` varchar(20) DEFAULT NULL,
   `content` text,
@@ -37,12 +36,10 @@ CREATE TABLE `post` (
   UNIQUE KEY `post_id_UNIQUE` (`post_id`),
   UNIQUE KEY `post_title_UNIQUE` (`title`),
   KEY `FK_post_topicid_idx` (`topicid`),
-  KEY `FK_post_username_idx` (`username`),
-  CONSTRAINT `FK_post_topicid` FOREIGN KEY (`topicid`) REFERENCES `topic` (`topic_id`),
-  CONSTRAINT `FK_post_username` FOREIGN KEY (`username`) REFERENCES `user` (`username`)
+  KEY `FK_post_username_idx` (`username`)
 );
 
-CREATE TABLE `reply` (
+CREATE TABLE reply (
   `reply_id` int NOT NULL AUTO_INCREMENT,
   `reply_content` text,
   `reply_author` int DEFAULT NULL,
@@ -50,9 +47,7 @@ CREATE TABLE `reply` (
   PRIMARY KEY (`reply_id`),
   UNIQUE KEY `reply_id_UNIQUE` (`reply_id`),
   KEY `FK_reply_userid_idx` (`reply_author`),
-  KEY `FK_reply_postid_idx` (`reply_originalpost`),
-  CONSTRAINT `FK_reply_postid` FOREIGN KEY (`reply_originalpost`) REFERENCES `post` (`post_id`),
-  CONSTRAINT `FK_reply_userid` FOREIGN KEY (`reply_author`) REFERENCES `user` (`user_id`)
+  KEY `FK_reply_postid_idx` (`reply_originalpost`)
 );
 
 # Create the app user and give it access to the database
